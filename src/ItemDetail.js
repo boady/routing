597b51a470c2fb22bcb55176c63b77000b2bdc7f
boady <roberttroxell86@gmail.com>
2019-07-24 12:13:32 -0400

    Update ItemDetail.js

--- src/ItemDetail.js
+++ src/ItemDetail.js
@@ -2,24 +2,26 @@ import React, {useState, useEffect} from 'react';
 import './App.css';
 import {Link} from 'react-router-dom';
 
-function ItemDetail() {
+function ItemDetail({ match }) {
 
   useEffect(() => {
     fetchItem();
+    console.log(match);
   },[]);
 
   const[item, setItem] = useState({});
 
   const fetchItem = async () => {
-    const fetchItem = await fetch(`https://fortnite-public-api.theapinetwork.com/prod09/item/get?ids=61ea3e9-8438e42-f53d351-e53a5ce`)
+    const fetchItem = await fetch(`https://fortnite-public-api.theapinetwork.com/prod09/item/get?ids=${match.params.id}`)
     const item = await fetchItem.json();
+    setItem(item);
 
     console.log(item);
   };
 
   return (
     <div>
-    <h1>Item</h1>
+    <h1>{item.name}</h1>
     </div>
   );
 }
