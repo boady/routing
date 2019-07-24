ed9a043c778a439bc6c4244c1499175470a012c5
boady <roberttroxell86@gmail.com>
2019-07-23 21:55:41 -0400

    Create About.js

---
+++ src/About.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function About() {
+  return (
+    <div className="App">
+    <h1> About Page </h1>
+  );
+}
+
+export default About;
