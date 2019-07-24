b630f93c0fe003f9066ade0c1b1b079ee43f01b7
boady <roberttroxell86@gmail.com>
2019-07-24 12:43:24 -0400

    Added ratings and number of voters

--- src/ItemDetail.js
+++ src/ItemDetail.js
@@ -26,6 +26,7 @@ function ItemDetail({ match }) {
     <div>
     <h1>{item.name}</h1>
     <h1>Rating {item.ratings.avgStars}</h1>
+    <h1>Number of Voters {item.ratings.numberVotes}</h1>
     <img src={item.images.transparent} alt=""/>
     </div>
   );
