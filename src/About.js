fcba27e6ef55c2eeffa0c215cec2bd91c2dbab6a
boady <roberttroxell86@gmail.com>
2019-07-24 20:47:24 -0400

    change logo

--- src/About.js
+++ src/About.js
@@ -1,10 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function About() {
   return (
+    <div>
     <h1> About Page </h1>
+    <p> Template React Nav and data fetching from a public api. The Shop tab will display the upcoming items on fornite with a picture and rating. </p>
+    </div>
   );
 }
 
